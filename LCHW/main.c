321559dfdf631d85585eef5dcab1320f284095d7
ahdpeeb <ahdpeeb.h@gmail.com>
2016-04-07 21:13:45 +0300

    project -> initial commit

---
+++ LCHW/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LCHW
+//
+//  Created by Nikola Andriiev on 07.04.16.
+//  Copyright © 2016 Anfriiev.Mykola. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
