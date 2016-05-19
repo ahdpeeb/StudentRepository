bfa7c988e21a8604d9bfcaa6d95d893bdd7e4c1a
ahdpeeb <ahdpeeb.h@gmail.com>
2016-05-19 19:41:44 +0300

    ANSAutoreleasePoolAddObject => procesing

--- Source/IDPAutoreleasingStack/ANSAutoreleasePool/ANSAutoreleasePool.c
+++ Source/IDPAutoreleasingStack/ANSAutoreleasePool/ANSAutoreleasePool.c
@@ -6,7 +6,81 @@
 //  Copyright © 2016 Anfriiev.Mykola. All rights reserved.
 //
 
+#include <assert.h>
+
 #include "ANSAutoreleasePool.h"
-#include "ANSAutoreleasingStack.h"
-#include "ANSObject.h"
-#include "ANSLinkedList.h"
+
+#pragma mark -
+#pragma mark Privat Declaration
+
+static const size_t kANSSizeOfStack = 512;
+
+static
+void ANSAutoreleasePoolSetCount(ANSAutoreleasePool *pool, uint64_t count);
+
+static
+uint64_t ANSAutoreleasePoolGetCount(ANSAutoreleasePool *pool);
+
+static
+void ANSAutoreleasePoolCountAddValue(ANSAutoreleasePool *pool, short value);
+
+static
+ANSLinkedList *ANSAutoreleasePoolGetList(ANSAutoreleasePool *pool);
+
+static
+void ANSAutoreleasePoolSetList(ANSAutoreleasePool *pool);
+
+#pragma mark -
+#pragma mark Publick Implementation
+
+void __ANSAutoreleasePoolDeallocate(ANSAutoreleasePool *pool) {
+    ANSAutoreleasePoolCleanUpPool(pool);
+    
+    ANSObjectRelease(pool->_list);
+    __ANSObjectDeallocate(pool);
+}
+
+ANSAutoreleasePool *ANSAutoreleasePoolCreatePool(void) {
+    ANSAutoreleasePool *pool = ANSObjectCreateWithType(ANSAutoreleasePool);
+    
+    return pool;
+}
+
+void ANSAutoreleasePoolAddObject(ANSAutoreleasePool *pool, void *object) {
+    assert(pool);
+    
+//    if() {
+//        ANSAutoreleasingStack stack
+//    }
+}
+
+void ANSAutoreleasePoolCleanUpPool(ANSAutoreleasePool *pool);
+
+#pragma mark -
+#pragma mark Privat Implementation
+
+void ANSAutoreleasePoolSetCount(ANSAutoreleasePool *pool, uint64_t count) {
+    ANSAssignSetter(pool, _count, count);
+}
+
+uint64_t ANSAutoreleasePoolGetCount(ANSAutoreleasePool *pool) {
+    assert(pool);
+    
+    return pool->_count;
+}
+
+void ANSAutoreleasePoolCountAddValue(ANSAutoreleasePool *pool, short value) {
+    uint64_t count = ANSAutoreleasePoolGetCount(pool);
+    ANSAutoreleasePoolSetCount(pool, count += value);
+}
+
+void ANSAutoreleasePoolSetList(ANSAutoreleasePool *pool) {
+    
+    ANSLinkedList *list = ANSObjectCreateWithType(ANSLinkedList);
+    ANSRetainSetter(pool, _list, list);
+}
+
+ANSLinkedList *ANSAutoreleasePoolGetList(ANSAutoreleasePool *pool) {
+    return pool->_list;
+}
+
--- Source/IDPAutoreleasingStack/ANSAutoreleasePool/ANSAutoreleasePool.h
+++ Source/IDPAutoreleasingStack/ANSAutoreleasePool/ANSAutoreleasePool.h
@@ -9,8 +9,28 @@
 #ifndef ANSAutoreleasePool_h
 #define ANSAutoreleasePool_h
 
+#include "ANSAutoreleasingStack.h"
+#include "ANSObject.h"
+#include "ANSLinkedList.h"
 
+typedef struct ANSAutoreleasePool ANSAutoreleasePool;
+struct ANSAutoreleasePool {
+    ANSObject *_super;
+    
+    ANSLinkedList *_list;
+    uint64_t _count;
+};
 
+extern
+void __ANSAutoreleasePoolDeallocate(ANSAutoreleasePool *pool);
 
+extern
+ANSAutoreleasePool *ANSAutoreleasePoolCreatePool(void);
+
+extern
+void ANSAutoreleasePoolAddObject(ANSAutoreleasePool *pool, void *object);
+
+extern
+void ANSAutoreleasePoolCleanUpPool(ANSAutoreleasePool *pool);
 
 #endif /* ANSAutoreleasePool_h */
